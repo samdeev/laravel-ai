3a4b4bbd416750ea8f2b985b05ffa1017435e4b9
Samuel Esplana <samesplana_dev@my.pisopay.com.ph>
2024-03-01 15:14:04 +0800

    E3 - Chat with AI from Command Line

---
+++ app/Console/Commands/StartChat.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\AI\Chat;
+use Illuminate\Console\Command;
+use function Laravel\Prompts\{outro, text, info, spin};
+
+class StartChat extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'chat';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Start chatting with OpenAI';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle(): void
+    {
+        $question = text(
+            label: 'How can I help you today?',
+            required: true
+        );
+
+        $chat = new Chat();
+
+        $response = spin(fn () => $chat->send($question), 'Generating response...');
+
+        info($response);
+
+        while ($question = text('Message ChatGPT...')) {
+
+            $response = spin(fn () => $chat->send($question), 'Generating response...');
+
+            info($response);
+        }
+
+    }
+}
--- config/services.php
+++ config/services.php
@@ -31,8 +31,4 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
-    'openai' => [
-        'secret' => env('OPENAI_SECRET')
-    ]
-
 ];
